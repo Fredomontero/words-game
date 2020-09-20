50b0a715c44ddfbcbeb09d6a2fb9741a6f1557d1
Fredomontero <christianmontero08@gmail.com>
2020-09-20 15:47:26 -0700

    Add integrationTests file

---
+++ src/integration.test.js
@@ -0,0 +1,26 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './redux/actions/index';
+
+describe('GuessWord action dispatcher', () => {
+  
+  describe('No guessed words', () => {
+    test('Updates state correctly for unsuccessful guess', () => {
+
+    });
+
+    test('updates state correctly for successful guess', () => {
+
+    });
+  });
+
+  describe('Some guessed words', () => {
+    test('Updates state correctly for unsuccessful guess', () => {
+
+    });
+
+    test('updates state correctly for successful guess', () => {
+
+    });
+  });
+
+});
---
+++ src/redux/reducers/guessWordsReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function guessedWordsReducer
+ * @param {array} state - array of guessed words
+ * @param {object} action - action to be reduced
+ * @return {array} - nwe guessedWords state
+ */
+export default (state, action) => {
+  return null;
+};
